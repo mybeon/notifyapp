1016acff5ce3490c9768cff6cb022dd07c396d38
hichamelb <hichamelb54@gmail.com>
2023-01-17 18:00:28 +0100

    renamed app to notify list app

--- ios/NotifylistappTests/NotifylistappTests.m
+++ ios/NotifylistappTests/NotifylistappTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface notifyAppTests : XCTestCase
+@interface NotifylistappTests : XCTestCase
 
 @end
 
-@implementation notifyAppTests
+@implementation NotifylistappTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
